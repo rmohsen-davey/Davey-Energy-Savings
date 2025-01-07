52e5c6f558c4b41a400736305ad168ed5fbdc031
rmohsen-davey <rmohsen@daveyeurope.eu>
2025-01-07 10:00:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <div 
       className="min-h-screen relative bg-fixed bg-contain bg-center sm:bg-contain sm:bg-top"
       style={{
-        backgroundImage: 'url("/davey-background.jpeg")',
+        backgroundImage: 'url("/davey-background.png")',
         backgroundPosition: 'top',
         backgroundRepeat: 'no-repeat'
       }}
